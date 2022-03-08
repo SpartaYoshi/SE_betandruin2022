b667e56a305ea4584a2bf7807257ee6a30d82ab9
nagor <nagor@192.168.1.21>
2022-03-08 18:32:13 +0100

    haciendo portal gui

--- src/main/java/gui/PrincipalPortalGUI.java
+++ src/main/java/gui/PrincipalPortalGUI.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PrincipalPortalGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtSelectAnOption;
 	
 
 	/**
@@ -36,8 +42,32 @@ public class PrincipalPortalGUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton signupButton = new JButton("Sign up");
+		signupButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// entrar a registerGUI
+			}
+		});
+		signupButton.setBounds(79, 115, 85, 21);
+		contentPane.add(signupButton);
+		
+		JButton loginButton = new JButton("Log in");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// entrar a loginGUI
+			}
+		});
+		loginButton.setBounds(254, 115, 85, 21);
+		contentPane.add(loginButton);
+		
+		txtSelectAnOption = new JTextField();
+		txtSelectAnOption.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtSelectAnOption.setText("Select an option:");
+		txtSelectAnOption.setBounds(152, 46, 121, 42);
+		contentPane.add(txtSelectAnOption);
+		txtSelectAnOption.setColumns(10);
 	}
-
 }
