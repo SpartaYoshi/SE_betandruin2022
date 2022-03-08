810563afef5560544894d084831e6884f35b49c2
nagor <nagor@192.168.1.21>
2022-03-08 18:49:10 +0100

    portal

--- src/main/java/gui/PortalGUI.java
+++ src/main/java/gui/PortalGUI.java
@@ -2,14 +2,20 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class PortalGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtSelectAnOption;
 	
 
 	/**
@@ -19,7 +25,7 @@ public class PortalGUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PortalGUI frame = new PortalGUI();
+					PrincipalPortalGUI frame = new PrincipalPortalGUI();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -36,8 +42,32 @@ public class PortalGUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton signupButton = new JButton("Sign up");
+		signupButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// entrar a registerGUI
+			}
+		});
+		signupButton.setBounds(79, 115, 85, 21);
+		contentPane.add(signupButton);
+		
+		JButton loginButton = new JButton("Log in");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// entrar a loginGUI
+			}
+		});
+		loginButton.setBounds(254, 115, 85, 21);
+		contentPane.add(loginButton);
+		
+		txtSelectAnOption = new JTextField();
+		txtSelectAnOption.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtSelectAnOption.setText("Select an option:");
+		txtSelectAnOption.setBounds(152, 46, 121, 42);
+		contentPane.add(txtSelectAnOption);
+		txtSelectAnOption.setColumns(10);
 	}
-
 }
