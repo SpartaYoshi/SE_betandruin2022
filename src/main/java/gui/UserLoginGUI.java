bcd24138479eeaaee8089facb3849d4559e7b104
Asier Septién <yoshimanbiz@gmail.com>
2022-03-08 19:20:03 +0100

    instrucciones para la creacion del sistema login

--- src/main/java/gui/UserLoginGUI.java
+++ src/main/java/gui/UserLoginGUI.java
@@ -76,9 +76,34 @@ public class UserLoginGUI extends JFrame {
 		
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				initWindow = new MainAdminGUI();
-				initWindow.setVisible(true);
-				BlFacade businesslogic = initWindow.getBusinessLogic();
+				initWindow = new MainAdminGUI();	// CAMBIAR POR "new MainUserGUI();" CUANDO HAGAIS LA CLASE
+				BlFacade bizlog = initWindow.getBusinessLogic();
+				
+				// Nota: siempre que hagas tratado de errores, haz try y catch.
+				//       crea getters que falten si no están en la clase domain.User
+				// 		 FailedLoginException: imprime en un rectangulito algo estilo "Login credentials were incorrect. Please try again"
+				
+				//1. pillar nombre de usuario del Textfield y mirar si existe en la DB
+					// si no existe, lanzar y catch: crea FailedLoginException
+					// si existe, guardalo en una variable
+				
+				//2. pillar contraseña del passwordfield y mirar si la contraseña es correcta (usando el usuario que acabas de guardar)
+					// si la contraseña no coincide, lanzar y catch: FailedLoginException
+				
+				//3. mirar si usuario es admin (ya existe un booleano como atributo pero falta el getter)
+					// if (user.isAdmin()) {
+					//				initWindow = new MainAdminGUI();
+					//				bizlog = initWindow.getBusinessLogic();
+					// }
+				
+				
+				//4. ahora si que sí
+				// initWindow.setVisible(true); 	
+				
+				
+				////////////////////////////////
+				// vinculación:		UserLoginGUI > MainUserGUI > businesslogic (BlFacade) > DataAccess
+				
 			}
 		});
 		
