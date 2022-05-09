7437995f10b3392e9523a3b84d49581f988eafad
ainhi <ainhizelegarreta@gmail.com>
2022-05-09 17:57:53 +0200

    creating Movement class

---
+++ src/main/java/domain/Movement.java
@@ -0,0 +1,63 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Date;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+public class Movement {
+    @Id
+    @XmlJavaTypeAdapter(IntegerAdapter.class)
+    @GeneratedValue
+    private int id;
+
+    private Double amount;
+    private Date date;
+    private String description;
+
+    public Movement(int id, Double amount, Date date) {
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Movement{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", date=" + date +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- src/main/java/uicontrollers/ShowMovementsController.java
+++ src/main/java/uicontrollers/ShowMovementsController.java
@@ -49,10 +49,6 @@ public class ShowMovementsController implements Controller{
         businessLogic = bl;
     }
 
-
-
-
-
     @FXML
     void initialize() {
 
