c271e1d925089ea1d3756a4f880a0d194dc828f1
nagorebravo <nbravo013@ikasle.ehu.eus>
2022-05-12 18:12:45 +0200

    edited dataaccess

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -612,7 +612,7 @@ public class DataAccess {
 
 	}
 
-
+	/*
 	public User editUserName(User who, String newUsername){
 		db.getTransaction().begin();
 		User dbUser=db.find(User.class, who.getUsername());
@@ -624,6 +624,7 @@ public class DataAccess {
 
 		return who;
 	}
+	*/
 
 
 	public User editPassWord(User who, String newPassword){
@@ -631,6 +632,7 @@ public class DataAccess {
 		User dbUser=db.find(User.class, who.getUsername());
 		dbUser.setUsername(newPassword);
 		who.setUsername(newPassword);
+		db.persist(dbUser);
 		db.getTransaction().commit();
 
 		System.out.println(">> DataAccess: password updated");
